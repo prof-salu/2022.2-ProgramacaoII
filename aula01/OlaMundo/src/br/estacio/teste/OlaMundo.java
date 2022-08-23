c3209518c94008c1425d0b90cb2af49262fac674
Salustiano Oliveira <saluorodrigues@gmail.com>
2022-08-23 12:38:56 -0300

    aula01

---
+++ aula01/OlaMundo/src/br/estacio/teste/OlaMundo.java
@@ -0,0 +1,15 @@
+package br.estacio.teste;
+
+/**
+ *
+ * @author aluno
+ */
+
+//ALT + MOUSE SCROLL ==> ZOOM
+public class OlaMundo {
+    //psvm + TAB ==> public static void main(String[] args){} 
+    public static void main(String[] args) {
+        //sout + TAB ==> System.out.println()
+        System.out.println("Olá mundo, Programação II!");
+    }
+}
